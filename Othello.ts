a5fdba87214c99cac0ee215e77d99956396f2221
AntiMelee <tmskiti@outlook.jp>
2021-05-20 14:57:44 +0900

    ひっくり返す処理途中

--- Othello.ts
+++ Othello.ts
@@ -1,9 +1,11 @@
 window.onload = function () { 
   let tarn:number = 0; //0 = game開始前 -1 = 黒 1 = 白
-  let othelloField:number[][] = new Array(8); // 石なし = 0 白 = 1 黒 = 2
+  let tarnCount:number = 0;
+  let othelloField:number[][] = new Array(10); // 石なし = 0 白 = -1 黒 = 1
+  let directionArray: number[][] = [[-1,-1],[-1,0],[-1,1],[0,1],[1,1],[1,0],[1,-1],[0,-1]];
   let i: number = 0;
-  while(i < 8){
-    othelloField[i] = new Array(8);
+  while(i < 10){
+    othelloField[i] = new Array(10);
     i++;
   }
 
@@ -13,23 +15,43 @@ window.onload = function () {
   bordReset();
   bordStats();
 
-  for(let x:number=0;x<8;x++){
-    for(let y:number=0;y<8;y++){
-      let clickEvent:HTMLElement = bord.rows[x].cells[y];
-      clickEvent.onclick = function(){
-        console.log(clickEvent.innerText); 
-        if(clickEvent.innerText === ""){ //divで要素を管理しているため全てからの判定　修正の必要
-          clickEvent.innerHTML = '<div class="Black"></div>';
-        }
-      }
-    }
+  //描画の必要性がない部分の削除
+  for(let i=0;i<36;i++){
+    let cant : HTMLElement = <HTMLElement>document.getElementsByClassName('cant')[i].parentNode;
+    cant.classList.add('dontSeen');
   }
 
-  function bordCreate(){
-    for(let j:number=0;j < 8;j++){
+  bord.addEventListener("click", (e) =>{
+    let clickCell: HTMLElement = <HTMLElement>e.target;
+    let clickRow: HTMLElement = <HTMLElement>clickCell.parentNode;
+    console.log("クリック");
+    console.log(clickRow.rowIndex,clickCell.cellIndex);// erroe が発生しているがセルは取れる
+    switch(tarn){
+      case -1:
+        break;
+      case 1:
+        console.log("黒のターン");
+        if(othelloField[clickRow.rowIndex][clickCell.cellIndex] === 0){
+          console.log("test");
+          canPut(clickRow.rowIndex,clickCell.cellIndex,1)
+          tarn = 2;
+        }
+        break;
+      case 2:
+        console.log("白のターン");
+        if(othelloField[clickRow.rowIndex][clickCell.cellIndex] === 0){
+          console.log("test");
+          tarn = 1;
+        }
+        break;
+    }
+  })
+
+  function bordCreate(){ //外側にindexの範囲外を避けるために大きく作る
+    for(let j:number=0;j < 10;j++){
       let tr : HTMLElement = document.createElement("tr");
       bord.appendChild(tr);
-      for(let k:number=0;k < 8;k++){
+      for(let k:number=0;k < 10;k++){
         let td : HTMLElement = document.createElement("td");
         tr.appendChild(td);
       }
@@ -37,35 +59,73 @@ window.onload = function () {
   }
 
   function bordReset(){
-    for(let x:number=0;x<8;x++){
-      for(let y:number=0;y<8;y++){
-        othelloField[x][y] = 0
+    for(let x:number=0;x<10;x++){
+      for(let y:number=0;y<10;y++){
+        othelloField[x][y] = 0;
       }
     }
-    othelloField[3][3] = 1;
+    othelloField[5][4] = -1;
+    othelloField[4][5] = -1;
     othelloField[4][4] = 1;
-    othelloField[4][3] = 2;
-    othelloField[3][4] = 2;
+    othelloField[5][5] = 1;
+    for(let r=0;r<10;r++){
+      othelloField[0][r] = 3;
+      othelloField[r][0] = 3;
+      othelloField[r][9] = 3;
+      othelloField[9][r] = 3;
+      console.log("banana");
+    }
     tarn = 1;
+    tarnCount = 0;
   }
 
   function bordStats(){
-    for(let x:number=0;x<8;x++){
-      for(let y:number=0;y<8;y++){
+    for(let x:number=0;x<10;x++){
+      for(let y:number=0;y<10;y++){
         let piece:string = "";
         switch(othelloField[x][y]){
           case 0:
             piece = "";
             break;
-          case 1:
-            piece = '<div class="Black"></div>';
+          case -1:
+            piece = '<div class="black"></div>';
             break;
-          case 2:
-            piece = '<div class="White"></div>';
+          case 1:
+            piece = '<div class="white"></div>';
             break;  
+          case 3:
+            piece = '<div class="cant"></div>'
         }
         bord.rows[x].cells[y].innerHTML = piece;
       }
     }
+  }
+  //012
+  //7@3
+  //654
+  function canPut(x:number,y:number,color:number){//color は操作している色
+    for(let i =0;i<8;i++){
+      if(othelloField[x+directionArray[i][0]][y+directionArray[i][1]] * -1 === color){
+        reverse(othelloField[x+directionArray[i][0]],[y+directionArray[i][1]],i,color)
+      }
+    }
+  }
+
+  function reverse(x,y,direction,color){
+    let reverseArray :number[][] = [];
+    let reverseCount :number = 0;
+    let stopFlag = false;
+        for(let i=1;i<9;i++){
+          let spotTmp:number = othelloField[x-(directionArray[direction][0] * i)][y-(directionArray[direction][1] * i)];
+          if(spotTmp === color * -1){
+            reverseArray.push([x-(directionArray[direction][0] * i)],[y-(directionArray[direction][1] * i)])
+            reverseCount++;
+          }else if(spotTmp === color){
+            if(reverseCount === 0){
+              stopFlag = true;
+            }
+            //ひっくり返すしょりお
+          }
+        }
   }
 }
--- othello.js
+++ othello.js
@@ -1,65 +1,91 @@
 window.onload = function () {
     var tarn = 0; //0 = game開始前 -1 = 黒 1 = 白
-    var othelloField = new Array(8); // 石なし = 0 白 = 1 黒 = 2
+    var tarnCount = 0;
+    var othelloField = new Array(10); // 石なし = 0 白 = -1 黒 = 1
     var i = 0;
-    while (i < 8) {
-        othelloField[i] = new Array(8);
+    while (i < 10) {
+        othelloField[i] = new Array(10);
         i++;
     }
     var bord = document.getElementById('othelloBord');
     bordCreate();
     bordReset();
     bordStats();
-    for (var x = 0; x < 8; x++) {
-        var _loop_1 = function (y) {
-            var clickEvent = bord.rows[x].cells[y];
-            clickEvent.onclick = function () {
-                console.log(clickEvent.innerText);
-                if (clickEvent.innerText === "") {
-                    clickEvent.innerHTML = '<div class="Black"></div>';
-                }
-            };
-        };
-        for (var y = 0; y < 8; y++) {
-            _loop_1(y);
-        }
+    //描画の必要性がない部分の削除
+    for (var i_1 = 0; i_1 < 36; i_1++) {
+        var cant = document.getElementsByClassName('cant')[i_1].parentNode;
+        cant.classList.add('dontSeen');
     }
+    bord.addEventListener("click", function (e) {
+        var clickCell = e.target;
+        var clickRow = clickCell.parentNode;
+        console.log("クリック");
+        console.log(clickRow.rowIndex, clickCell.cellIndex); // erroe が発生しているがセルは取れる
+        switch (tarn) {
+            case -1:
+                break;
+            case 1:
+                console.log("黒のターン");
+                if (othelloField[clickRow.rowIndex][clickCell.cellIndex] === 0) {
+                    console.log("test");
+                    tarn = 2;
+                }
+                break;
+            case 2:
+                console.log("白のターン");
+                if (othelloField[clickRow.rowIndex][clickCell.cellIndex] === 0) {
+                    console.log("test");
+                    tarn = 1;
+                }
+                break;
+        }
+    });
     function bordCreate() {
-        for (var j = 0; j < 8; j++) {
+        for (var j = 0; j < 10; j++) {
             var tr = document.createElement("tr");
             bord.appendChild(tr);
-            for (var k = 0; k < 8; k++) {
+            for (var k = 0; k < 10; k++) {
                 var td = document.createElement("td");
                 tr.appendChild(td);
             }
         }
     }
     function bordReset() {
-        for (var x = 0; x < 8; x++) {
-            for (var y = 0; y < 8; y++) {
+        for (var x = 0; x < 10; x++) {
+            for (var y = 0; y < 10; y++) {
                 othelloField[x][y] = 0;
             }
         }
-        othelloField[3][3] = 1;
+        othelloField[5][4] = -1;
+        othelloField[4][5] = -1;
         othelloField[4][4] = 1;
-        othelloField[4][3] = 2;
-        othelloField[3][4] = 2;
+        othelloField[5][5] = 1;
+        for (var r = 0; r < 10; r++) {
+            othelloField[0][r] = 3;
+            othelloField[r][0] = 3;
+            othelloField[r][9] = 3;
+            othelloField[9][r] = 3;
+            console.log("banana");
+        }
         tarn = 1;
+        tarnCount = 0;
     }
     function bordStats() {
-        for (var x = 0; x < 8; x++) {
-            for (var y = 0; y < 8; y++) {
+        for (var x = 0; x < 10; x++) {
+            for (var y = 0; y < 10; y++) {
                 var piece = "";
                 switch (othelloField[x][y]) {
                     case 0:
                         piece = "";
                         break;
+                    case -1:
+                        piece = '<div class="black"></div>';
+                        break;
                     case 1:
-                        piece = '<div class="Black"></div>';
+                        piece = '<div class="white"></div>';
                         break;
-                    case 2:
-                        piece = '<div class="White"></div>';
-                        break;
+                    case 3:
+                        piece = '<div class="cant"></div>';
                 }
                 bord.rows[x].cells[y].innerHTML = piece;
             }
